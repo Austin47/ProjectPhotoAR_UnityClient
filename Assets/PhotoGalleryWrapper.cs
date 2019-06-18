57897956edc3499322fad172dea6eab9e9b8455e
Austin47 <austintaylorx@gmail.com>
2019-06-18 06:50:19 -0700

    Add photo gallery, display photo select, woot!!!

---
+++ Assets/PhotoGalleryWrapper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PhotoGalleryWrapper : MonoBehaviour
+{
+    private static AndroidJavaObject context;
+    private static AndroidJavaObject Context
+    {
+        get
+        {
+            if (context == null)
+            {
+                using (AndroidJavaObject unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+                {
+                    context = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
+                }
+            }
+
+            return context;
+        }
+    }
+
+    private static AndroidJavaObject androidGallery;
+    private static AndroidJavaObject AndroidGallery
+    {
+        get
+        {
+            if (androidGallery == null)
+            {
+                androidGallery = new AndroidJavaObject("com.absurd.photogallery.AndroidGalleryJ");
+            }
+
+            return androidGallery;
+        }
+    }
+
+    public void PickPhoto()
+    {
+        AndroidGallery.CallStatic("Call", Context);
+    }
+}
+
