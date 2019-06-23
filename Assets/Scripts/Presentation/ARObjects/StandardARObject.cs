4bc7e9810d083eb0ff99fdcc7ae9f67459da0ff9
Austin47 <austintaylorx@gmail.com>
2019-06-22 18:18:18 -0700

    AR object position fix

--- Assets/Scripts/Presentation/ARObjects/StandardARObject.cs
+++ Assets/Scripts/Presentation/ARObjects/StandardARObject.cs
@@ -16,6 +16,7 @@ namespace Presentation.ARObjectSpawner
             var textureSize = new Vector2(texture.width, texture.height);
             var scale = textureSize.GetEnvelopeToValue(1);
             rend.transform.localScale = new Vector3(scale.x, scale.y, 1);
+            rend.transform.localPosition = new Vector3(0, scale.y / 2, 0);
         }
     }
 }
