2e88115ce868614df796a410f28d013f53eedfb9
Austin47 <austintaylorx@gmail.com>
2019-06-22 21:17:11 -0700

    Camera Capture animation

---
+++ Assets/Scripts/Presentation/CameraCapture/CameraCaptureTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Presentation.CameraCaptureService
+{
+    public class CameraCaptureTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent onCapture;
+
+        public void CaptrueImage()
+        {
+            onCapture.Invoke();
+        }
+    }
+}
+
+
