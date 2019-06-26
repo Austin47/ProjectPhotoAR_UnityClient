5805e1ba4508984ee6b306ab81c33fb7dd822c8f
Austin47 <austintaylorx@gmail.com>
2019-06-26 12:03:39 -0700

    Update project settings, add build code

---
+++ Assets/Scripts/Infrastructure/Editor/Builder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Infrastructure.EditorHelpers
+{
+    public class Builder
+    {
+        [MenuItem("Tools/AutoBuilder/Android")]
+        public static void BuildDevForAndroid()
+        {
+            List<string> scenes = new List<string>();
+            foreach (var s in EditorBuildSettings.scenes)
+            {
+                scenes.Add(s.path);
+            }
+            string buildPath = "Builds/Android/Development";
+            if (!Directory.Exists(buildPath))
+                Directory.CreateDirectory(buildPath);
+
+            string buildName = $"v{PlayerSettings.bundleVersion}_b{PlayerSettings.Android.bundleVersionCode}.apk";    
+            BuildPipeline.BuildPlayer(scenes.ToArray(), $"{buildPath}/{buildName}", BuildTarget.Android, BuildOptions.Development);
+        }
+    }
+}
+
