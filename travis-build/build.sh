009f3c3cf9d96fa8db6c455af6a774b572865b07
Austin47 <austintaylorx@gmail.com>
2019-06-26 12:03:57 -0700

    Add travis logic

---
+++ travis-build/build.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity-android.log
+
+
+ERROR_CODE=0
+echo "Items in project path ($PROJECT_PATH):"
+ls "$PROJECT_PATH"
+
+
+echo "Building project for Windows..."
+mkdir $UNITY_BUILD_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$PROJECT_PATH" \
+  -buildTarget "Android" \
+  -executeMethod "Infrastructure.EditorHelpers.Builder.BuildDevForAndroid"
+  -quit \
+  | tee "$LOG_FILE"
+  
+if [ $? = 0 ] ; then
+  echo "Building Windows exe completed successfully."
+  ERROR_CODE=0
+else
+  echo "Building Windows exe failed. Exited with $?."
+  ERROR_CODE=1
+fi
+
+#echo 'Build logs:'
+#cat $LOG_FILE
+
+echo "Finishing with code $ERROR_CODE"
+exit $ERROR_CODE
