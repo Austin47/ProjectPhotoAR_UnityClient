4e8d7ce8e5c91268e1501b4d32b55429dfea7c01
Austin47 <austintaylorx@gmail.com>
2019-06-27 14:45:47 -0700

    Add Test check logic

--- travis-build/run-tests.sh
+++ travis-build/run-tests.sh
@@ -1,4 +1,33 @@
 #! /bin/sh
 
-echo "Skipping testing. No Tests implemented yet..."
-exit 0
+PROJECT_PATH=$(pwd)
+UNITY_BUILD_DIR=$(pwd)/Build
+
+ERROR_CODE=1
+
+echo "Running editor test..."
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$PROJECT_PATH" \
+  -buildTarget "Android" \
+  -username "$UNITYEMAIL" \
+  -password "$UNITYPASSWORD" \
+  -serial "$UNITYKEY" \
+  -runEditorTests |
+  tee "$LOG_FILE"
+
+if [ $? = 0 ]; then
+  echo "Editor tests Passed."
+  ERROR_CODE=0
+else
+  echo "One or more Editor test failed $?."
+  ERROR_CODE=1
+fi
+
+echo "return license"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense
+
+exit $ERROR_CODE
