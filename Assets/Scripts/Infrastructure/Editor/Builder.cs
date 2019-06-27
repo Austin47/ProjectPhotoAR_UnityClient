911510a5b8d8bb8a1b2c570ceb0a3c7435ceb8a6
Austin47 <austintaylorx@gmail.com>
2019-06-26 21:36:40 -0700

    disable build to test build error

--- Assets/Scripts/Infrastructure/Editor/Builder.cs
+++ Assets/Scripts/Infrastructure/Editor/Builder.cs
@@ -11,17 +11,18 @@ namespace Infrastructure.EditorHelpers
         [MenuItem("Tools/AutoBuilder/Android")]
         public static void BuildDevForAndroid()
         {
-            List<string> scenes = new List<string>();
-            foreach (var s in EditorBuildSettings.scenes)
-            {
-                scenes.Add(s.path);
-            }
-            string buildPath = "Builds/Android/Development";
-            if (!Directory.Exists(buildPath))
-                Directory.CreateDirectory(buildPath);
+            // Temporarily comment out code so test fails
+            // List<string> scenes = new List<string>();
+            // foreach (var s in EditorBuildSettings.scenes)
+            // {
+            //     scenes.Add(s.path);
+            // }
+            // string buildPath = "Builds/Android/Development";
+            // if (!Directory.Exists(buildPath))
+            //     Directory.CreateDirectory(buildPath);
 
-            string buildName = $"v{PlayerSettings.bundleVersion}_b{PlayerSettings.Android.bundleVersionCode}.apk";    
-            BuildPipeline.BuildPlayer(scenes.ToArray(), $"{buildPath}/{buildName}", BuildTarget.Android, BuildOptions.Development);
+            // string buildName = $"v{PlayerSettings.bundleVersion}_b{PlayerSettings.Android.bundleVersionCode}.apk";    
+            // BuildPipeline.BuildPlayer(scenes.ToArray(), $"{buildPath}/{buildName}", BuildTarget.Android, BuildOptions.Development);
         }
     }
 }
