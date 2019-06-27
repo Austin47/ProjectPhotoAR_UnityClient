5488c6b61bcbc738e4b63013d0778d5b41836ded
Austin47 <austintaylorx@gmail.com>
2019-06-26 21:36:12 -0700

    Check to make sure android build exists

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -3,12 +3,11 @@
 PROJECT_PATH=$(pwd)
 UNITY_BUILD_DIR=$(pwd)/Build
 LOG_FILE=$UNITY_BUILD_DIR/unity-android.log
-
+UNITY_BUILD_APK=$PROJECT_PATH/Builds/Android/Development
 
 ERROR_CODE=0
 echo "Items in project path ($PROJECT_PATH):"
 ls "$PROJECT_PATH"
-
 
 echo "Building project for Android..."
 mkdir $UNITY_BUILD_DIR
@@ -23,12 +22,17 @@ mkdir $UNITY_BUILD_DIR
   -password "$UNITYPASSWORD" \
   -serial "$UNITYKEY" \
   -executeMethod "Infrastructure.EditorHelpers.Builder.BuildDevForAndroid" \
-  -quit \
-  | tee "$LOG_FILE"
-  
-if [ $? = 0 ] ; then
-  echo "Building Android apk completed successfully."
-  ERROR_CODE=0
+  -quit |
+  tee "$LOG_FILE"
+
+if [ $? = 0 ]; then
+  if [ -d UNITY_BUILD_APK ]; then
+    echo "Building Android apk completed successfully."
+    ERROR_CODE=0
+  else
+    echo "Building Android apk failed. File not pressent"
+    ERROR_CODE=1
+  fi
 else
   echo "Building Android apk failed. Exited with $?."
   ERROR_CODE=1
