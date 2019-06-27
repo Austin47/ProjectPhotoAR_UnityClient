bc4ef7aa767a9a61d6c4bba2273e1ab41229891d
Austin47 <austintaylorx@gmail.com>
2019-06-26 22:45:08 -0700

    Travis now looks for the built apk
    Fails if not found

--- Assets/Scripts/Infrastructure/Editor/Builder.cs
+++ Assets/Scripts/Infrastructure/Editor/Builder.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 namespace Infrastructure.EditorHelpers
 {
@@ -20,8 +18,28 @@ namespace Infrastructure.EditorHelpers
             if (!Directory.Exists(buildPath))
                 Directory.CreateDirectory(buildPath);
 
-            string buildName = $"v{PlayerSettings.bundleVersion}_b{PlayerSettings.Android.bundleVersionCode}.apk";    
+            string buildName = GetBuildName();
             BuildPipeline.BuildPlayer(scenes.ToArray(), $"{buildPath}/{buildName}", BuildTarget.Android, BuildOptions.Development);
+        }
+
+        private static string GetBuildName()
+        {
+            string name = GetArg("devName");
+            return !string.IsNullOrWhiteSpace(name) ? name : $"v{PlayerSettings.bundleVersion}_b{PlayerSettings.Android.bundleVersionCode}.apk";
+        }
+
+        // source: https://stackoverflow.com/questions/39843039/game-development-how-could-i-pass-command-line-arguments-to-a-unity-standalo#answer-45578115
+        private static string GetArg(string name)
+        {
+            var args = System.Environment.GetCommandLineArgs();
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == name && args.Length > i + 1)
+                {
+                    return args[i + 1];
+                }
+            }
+            return null;
         }
     }
 }
--- travis-build/build.sh
+++ travis-build/build.sh
@@ -3,7 +3,9 @@
 PROJECT_PATH=$(pwd)
 UNITY_BUILD_DIR=$(pwd)/Build
 LOG_FILE=$UNITY_BUILD_DIR/unity-android.log
-UNITY_BUILD_APK=$PROJECT_PATH/Builds/Android/Development
+UNITY_BUILD_APK_NAME=dev_travis.apk
+UNITY_BUILD_APK=$PROJECT_PATH/Builds/Android/Development/$UNITY_BUILD_APK_NAME
+
 
 ERROR_CODE=0
 echo "Items in project path ($PROJECT_PATH):"
@@ -12,6 +14,7 @@ ls "$PROJECT_PATH"
 echo "Building project for Android..."
 mkdir $UNITY_BUILD_DIR
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  --args devName $UNITY_BUILD_APK_NAME
   -batchmode \
   -nographics \
   -silent-crashes \
@@ -26,11 +29,11 @@ mkdir $UNITY_BUILD_DIR
   tee "$LOG_FILE"
 
 if [ $? = 0 ]; then
-  if [ -d $UNITY_BUILD_APK ]; then
+  if [ -f $UNITY_BUILD_APK ]; then
     echo "Building Android apk completed successfully."
     ERROR_CODE=0
   else
-    echo "Building Android apk failed. File not pressent"
+    echo "Building Android apk failed. Apk not found, check logs for possible build failure."
     ERROR_CODE=1
   fi
 else
